dbc88b89d3a47d416cd77acbaecf7afb4b879ddf
fath09 <56144971+Aharuto09@users.noreply.github.com>
2023-08-03 13:55:45 +0700

    Add files via upload

--- src/main/java/com/telkomedika/telkomedikaapi/controller/AuthController.java
+++ src/main/java/com/telkomedika/telkomedikaapi/controller/AuthController.java
@@ -6,10 +6,7 @@ import com.telkomedika.telkomedikaapi.model.response.authResponse;
 import com.telkomedika.telkomedikaapi.service.accountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -29,5 +26,9 @@ public class AuthController {
             @RequestBody authRequest request
     ) {
         return ResponseEntity.ok(accountservice.authenticate(request));
+    }
+    @GetMapping("/test")
+    public ResponseEntity testing(){
+        return ResponseEntity.ok("Test Hello World");
     }
 }
